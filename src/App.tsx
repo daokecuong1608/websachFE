b4898740a571c8da5609bfed714bf554ccea4292
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-09-15 15:12:20 +0700

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './layout/header-footer/Navbar';
+import Footer from './layout/header-footer/Footer';
 
 function App() {
   return (
     <div className="App">
-
+      {/* thanh dieu huong */}
       <Navbar />
+      {/* chan Trang */}
+      <Footer />
     </div>
   );
 }
